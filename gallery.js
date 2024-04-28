19601d5b0dd0cae05d80076101be2141fd2c3ea8
br4hm <haehnlein.alexander@gmx.de>
2024-04-28 15:54:40 +0200

    refactor: function ..toLeft and ..toRight in full size works fine now

--- gallery.js
+++ gallery.js
@@ -1,11 +1,12 @@
 const imgContainer = document.getElementById("imgContainer");
+const fullSizeImgContainer = document.getElementById("fullSizeImgContainer");
 let animalImages = [];
-
 buildAnimalImagesArray();
+console.table(animalImages);
 
 function buildAnimalImagesArray() {
   for (let i = 0; i < 21; i++) {
-    animalImages.push(`./src/img/img${[i]}.jpg`);
+    animalImages.push(`./src/img/img${i}.jpg`);
   }
   loadImages();
 }
@@ -13,9 +14,84 @@ function buildAnimalImagesArray() {
 function loadImages() {
   for (let i = 0; i < animalImages.length; i++) {
     imgContainer.innerHTML += `
-        <img src="./src/img/img${[
-          i,
-        ]}.jpg" alt="Image from a animal" id="img0" />
+        <img onclick="openFullSizeImg(${i})"src="${animalImages[i]}" alt="Image from a animal" id="img${i}" />
         `;
   }
+}
+
+function openFullSizeImg(i) {
+  fullSizeImgContainer.classList.remove("d-none");
+  openFullSizeImgHTML(i);
+}
+
+function closeFullSizeImg() {
+  fullSizeImgContainer.classList.add("d-none");
+}
+
+function fullSizeImgToRight(i) {
+  if (i < 21) {
+    fullSizeImgToRightHTML(i);
+  } else {
+    i = 0;
+    fullSizeImgToRight(i);
+  }
+}
+
+function fullSizeImgToLeft(i) {
+  if (i > -1) {
+    fullSizeImgToLeftHTML(i);
+  } else {
+    i = animalImages.length - 1;
+    fullSizeImgToRight(i);
+  }
+}
+
+// +++ HTML +++ //
+
+function openFullSizeImgHTML(i) {
+  fullSizeImgContainer.innerHTML = /*html*/ `
+        <div class="full-size-btn-container" id="fullSizeBtnContainer">
+            <div class="x-btn btn-hover" onclick="closeFullSizeImg()">&times;</div>
+              <div onclick="fullSizeImgToLeft(${
+                i - 1
+              })"class="arrow left-arrow btn-hover">&laquo;</div>
+              <div onclick="fullSizeImgToRight(${
+                i + 1
+              })" class="arrow right-arrow btn-hover" id="rightArrow"">&raquo;</div>
+        </div>
+            <img onclick="closeFullSizeImg()" src="${animalImages[i]}" alt="" />
+  `;
+  return;
+}
+
+function fullSizeImgToRightHTML(i) {
+  fullSizeImgContainer.innerHTML = /*html*/ `
+    <div class="full-size-btn-container" id="fullSizeBtnContainer">
+        <div class="x-btn btn-hover" onclick="closeFullSizeImg()">&times;</div>
+          <div onclick="fullSizeImgToLeft(${
+            i - 1
+          })"class="arrow left-arrow btn-hover">&laquo;</div>
+          <div onclick="fullSizeImgToRight(${
+            i + 1
+          })" class="arrow right-arrow btn-hover" id="rightArrow">&raquo;</div>
+    </div>
+        <img onclick="closeFullSizeImg()" src="${animalImages[i]}" alt="" />
+`;
+  return;
+}
+
+function fullSizeImgToLeftHTML(i) {
+  fullSizeImgContainer.innerHTML = /*html*/ `
+    <div class="full-size-btn-container" id="fullSizeBtnContainer">
+      <div class="x-btn btn-hover" onclick="closeFullSizeImg()">&times;</div>
+          <div onclick="fullSizeImgToLeft(${
+            i - 1
+          })" class="arrow left-arrow btn-hover">&laquo;</div>
+          <div onclick="fullSizeImgToRight(${
+            i + 1
+          })" class="arrow right-arrow btn-hover" id="rightArrow">&raquo;</div>
+    </div>
+          <img onclick="closeFullSizeImg()" src="${animalImages[i]}" alt="" />
+          `;
+  return;
 }
